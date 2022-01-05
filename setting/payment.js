21e287f4c2f285e1b144b1ff3c67fba595c2436d
RizzGg <aharam662@gmail.com>
2022-01-05 12:46:15 +0700

    Guti

---
+++ setting/payment.js
@@ -0,0 +1,9 @@
+exports.payment = (prefix) => {
+return `╭──「 *PAYMENT / DONASI* 」
+│
+├❏ Gopay : _Tuh Kak Scan Qrnya / 085606026618_
+├❏ Dana : _Tidak Tersedia_
+├❏ Ovo : _Tidak Tersedia_
+│
+╰──`
+}
---
+++ setting/sewabot.js
@@ -0,0 +1,15 @@
+exports.sewabot = (prefix) => {
+return `╭──「 *SEWABOT* 」
+│
+├❏ 1 MINGGU : 1.000 ( 1K )
+├❏ 1 BULAN : 5.000( 5K )
+├❏ PERMANEN : 10.000 ( 10K )
+│
+├──「 *PREMIUM* 」
+│
+├❏ 1 MINGGU : 3.000 ( 3K )
+├❏ 1 BULAN : 10.000 ( 10K )
+├❏ PERMANEN : 15.000 ( 15K )
+│
+╰──「 *${botname}* 」`
+}
